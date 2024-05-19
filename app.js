11466f086bc2b54079e0b26ce06a938904e75379
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 14:00:17 +0700

    js lab 39

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-// write code here
+if (-1 || 0) alert('first');             // (true || false)=> true alert=> first
+if (-1 && 0) alert('second');            // (true && false) => false => not alert
+if (null || (-1 && 1)) alert('third');   //(false || (true && true)) => alert => third
